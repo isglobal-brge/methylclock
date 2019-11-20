53ea325ffd23dc37a7f6cd029066fc2278270f30
JUAN RAMÓN <jrgonzalez@ISGLOBAL.LAN>
2019-11-20 12:06:33 +0100

    bug fix DNAmGA - Bohlin

--- R/DNAmGA.R
+++ R/DNAmGA.R
@@ -96,7 +96,7 @@ DNAmGA <- function(x, toBetas=FALSE,
   
 # --------------> Bohlin
   
-   bohlin <- try(GAprediction::predictGA(cpgs.imp, transpose=FALSE)[,1]/52, TRUE)
+   bohlin <- try(GAprediction::predictGA(cpgs.imp, transp=FALSE)[,1]/52, TRUE)
    if (inherits(bohlin, "try-error"))
      bohlin <- rep(NA, nrow(cpgs.imp))
    Bohlin <- data.frame(id = rownames(cpgs.imp),

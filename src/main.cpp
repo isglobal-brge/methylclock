2f85533ae5aa9c55310ec362c09f8c2145ecb9a8
dpelegri <dpelegri@gmail.com>
2021-07-02 08:37:03 +0200

    added methylclockData - vignette

--- src/main.cpp
+++ src/main.cpp
@@ -3,32 +3,14 @@
 //
 // MATLAB Coder version            : 3.2
 // C/C++ source code generated on  : 19-Oct-2018 17:06:17
+// 
+// 
+//    NOTE : Adapted to R using C++ with Rcpp
 //
 
-//***********************************************************************
-// This automatically generated example C main file shows how to call
-// entry-point functions that MATLAB Coder generated. You must customize
-// this file for your application. Do not modify this file directly.
-// Instead, make a copy of this file, modify it, and integrate it into
-// your development environment.
-//
-// This file initializes entry-point function arguments to a default
-// size and value before calling the entry-point functions. It does
-// not store or use any values returned from the entry-point functions.
-// If necessary, it does pre-allocate memory for returned values.
-// You can use this file as a starting point for a main function that
-// you can deploy in your application.
-//
-// After you copy the file, and before you deploy it, you must make the
-// following changes:
-// * For variable-size function arguments, change the example sizes to
-// the sizes that your application requires.
-// * Change the example values of function arguments to the values that
-// your application requires.
-// * If the entry-point functions return values, store these values or
-// otherwise use them as required by your application.
-//
-//***********************************************************************
+
+
+
 // Include Files
 #include<Rcpp.h>
 
@@ -95,12 +77,14 @@ static float argInit_real_T()
 // [[Rcpp::export]]
 Rcpp::NumericVector main_NewModel1Clean(Rcpp::RObject odata )
 {
+  
+  NewModel1Clean_initialize();
 
   Rcpp::NumericMatrix data = Rcpp:: as<Rcpp::NumericMatrix>(odata);
   
   emxArray_real_T *x1;
   int idat = 353;
-  int icount= (data.length() / idat);
+  int icount = (data.length() / idat);
   double b_y1[icount];
 
   x1 = argInit_1xUnbounded_real_T(2, idat, icount, data);
@@ -116,6 +100,8 @@ Rcpp::NumericVector main_NewModel1Clean(Rcpp::RObject odata )
     age[idq] = b_y1[idq];
   }
 
+  NewModel1Clean_terminate();
+  
   return Rcpp::wrap(age);
 
 }
